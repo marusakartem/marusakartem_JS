ca2ed94d50975d6b9613584d72c6406035b06fcf
marusakartem <marusakartem310@gmail.com>
2024-05-11 20:42:33 +0300

    Finished Task 3

--- script.js
+++ script.js
@@ -1,49 +1,53 @@
-const todos = [];
 const list = document.getElementById('todo-list');
 const itemCountSpan = document.getElementById('item-count');
 const uncheckedCountSpan = document.getElementById('unchecked-count');
+const todosKey = 'todosAppItems';
+let todos = JSON.parse(localStorage.getItem(todosKey)) || [];
+
+function saveTodos() {
+    localStorage.setItem(todosKey, JSON.stringify(todos));
+}
 
 function newTodo() {
-    const text = prompt("Enter new TODO:");
-    if (text) {
-        const todo = { id: Date.now(), text, completed: false };
-        todos.push(todo);
+    const task = prompt("Enter new TODO:");
+    if (task) {
+        const newTodoId = todos.length === 0 ? 1 : todos[todos.length - 1].id + 1;
+        const newTodo = { id: newTodoId, text: task, completed: false };
+        todos.push(newTodo);
         render();
-        updateCounter();
+        updateCounters();
+        saveTodos();
     }
 }
 
 function renderTodo(todo) {
     const checked = todo.completed ? 'checked' : '';
-    const textStyle = todo.completed ? 'text-decoration-line-through text-success' : '';
+    const textStyle = todo.completed ? 'text-success text-decoration-line-through' : '';
     return `
-        <li class="list-group-item" id="${todo.id}">
+        <li class="list-group-item">
             <input type="checkbox" class="form-check-input me-2" ${checked} onclick="checkTodo(${todo.id})" />
-            <label class="${textStyle}">${todo.text}</label>
+            <span class="${textStyle}">${todo.text}</span>
             <button class="btn btn-danger btn-sm float-end" onclick="deleteTodo(${todo.id})">delete</button>
         </li>
     `;
 }
 
 function render() {
-    const todoItems = todos.map(renderTodo).join('');
-    list.innerHTML = todoItems;
+    list.innerHTML = todos.map(renderTodo).join('');
 }
 
-function updateCounter() {
-    const totalCount = todos.length;
-    const uncheckedCount = todos.filter(todo => !todo.completed).length;
-    itemCountSpan.textContent = totalCount;
-    uncheckedCountSpan.textContent = uncheckedCount;
+function updateCounters() {
+    const totalTodos = todos.length;
+    const uncheckedTodos = todos.filter(todo => !todo.completed).length;
+    itemCountSpan.textContent = totalTodos;
+    uncheckedCountSpan.textContent = uncheckedTodos;
 }
 
 function deleteTodo(id) {
-    const index = todos.findIndex(todo => todo.id === id);
-    if (index > -1) {
-        todos.splice(index, 1);
-        render();
-        updateCounter();
-    }
+    todos = todos.filter(todo => todo.id !== id);
+    render();
+    updateCounters();
+    saveTodos();
 }
 
 function checkTodo(id) {
@@ -51,10 +55,10 @@ function checkTodo(id) {
     if (todo) {
         todo.completed = !todo.completed;
         render();
-        updateCounter();
+        updateCounters();
+        saveTodos();
     }
 }
 
 render();
-updateCounter();
-
+updateCounters();
