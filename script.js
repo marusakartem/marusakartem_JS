33ce09a00f51e13831a86fab174b163504c6d10f
marusakartem <marusakartem310@gmail.com>
2024-05-26 23:34:16 +0300

    Complete PR9

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 const list = document.getElementById('todo-list');
 const itemCountSpan = document.getElementById('item-count');
 const uncheckedCountSpan = document.getElementById('unchecked-count');
-const todosKey = 'todosAppItems';
-let todos = JSON.parse(localStorage.getItem(todosKey)) || [];
+const firebaseUrl = `https://todoapp-ca5da-default-rtdb.firebaseio.com/todos.json`;
+let todos = [];
 
-function saveTodos() {
-    localStorage.setItem(todosKey, JSON.stringify(todos));
+function fetchTodos() {
+    fetch(firebaseUrl)
+        .then(response => response.json())
+        .then(data => {
+            todos = data ? Object.keys(data).map(key => ({ id: key, ...data[key] })) : [];
+            render();
+            updateCounters();
+        })
+        .catch(error => console.error('Error fetching todos:', error));
+}
+
+function addTodoToDB(todo) {
+    return fetch(firebaseUrl, {
+        method: 'POST',
+        body: JSON.stringify(todo),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json());
 }
 
 function newTodo() {
     const task = prompt("Enter new TODO:");
     if (task) {
-        const newTodoId = todos.length === 0 ? 1 : todos[todos.length - 1].id + 1;
-        const newTodo = { id: newTodoId, text: task, completed: false };
-        todos.push(newTodo);
-        render();
-        updateCounters();
-        saveTodos();
+        const newTodo = { text: task, completed: false };
+        addTodoToDB(newTodo).then(data => {
+            newTodo.id = data.name;
+            todos.push(newTodo);
+            render();
+            updateCounters();
+        }).catch(error => console.error('Error adding todo:', error));
     }
 }
 
@@ -25,9 +43,9 @@ function renderTodo(todo) {
     const textStyle = todo.completed ? 'text-success text-decoration-line-through' : '';
     return `
         <li class="list-group-item">
-            <input type="checkbox" class="form-check-input me-2" ${checked} onclick="checkTodo(${todo.id})" />
+            <input type="checkbox" class="form-check-input me-2" ${checked} onclick="checkTodo('${todo.id}')" />
             <span class="${textStyle}">${todo.text}</span>
-            <button class="btn btn-danger btn-sm float-end" onclick="deleteTodo(${todo.id})">delete</button>
+            <button class="btn btn-danger btn-sm float-end" onclick="deleteTodo('${todo.id}')">delete</button>
         </li>
     `;
 }
@@ -43,22 +61,39 @@ function updateCounters() {
     uncheckedCountSpan.textContent = uncheckedTodos;
 }
 
+function deleteTodoFromDB(id) {
+    return fetch(`https://todoapp-ca5da-default-rtdb.firebaseio.com/todos.json/${id}.json`, {
+        method: 'DELETE'
+    });
+}
+
 function deleteTodo(id) {
-    todos = todos.filter(todo => todo.id !== id);
-    render();
-    updateCounters();
-    saveTodos();
+    deleteTodoFromDB(id).then(() => {
+        todos = todos.filter(todo => todo.id !== id);
+        render();
+        updateCounters();
+    }).catch(error => console.error('Error deleting todo:', error));
+}
+
+function updateTodoInDB(id, updatedTodo) {
+    return fetch(`https://todoapp-ca5da-default-rtdb.firebaseio.com/todos.json/${id}.json`, {
+        method: 'PATCH',
+        body: JSON.stringify(updatedTodo),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
 
 function checkTodo(id) {
     const todo = todos.find(todo => todo.id === id);
     if (todo) {
         todo.completed = !todo.completed;
-        render();
-        updateCounters();
-        saveTodos();
+        updateTodoInDB(id, { completed: todo.completed }).then(() => {
+            render();
+            updateCounters();
+        }).catch(error => console.error('Error updating todo:', error));
     }
 }
 
-render();
-updateCounters();
+fetchTodos();
